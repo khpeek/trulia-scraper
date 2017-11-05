b71a01ecbc24c98ad052518125029e9701b3cd2a
Kurt Peek <kurt.peek@gmail.com>
2017-11-04 19:17:07 -0700

    Updated the parsing of the number of pages to scrape. Also fixed the location of the feed export file relative to the Scrapy project's root directory

--- trulia_scraper/spiders/trulia_sold.py
+++ trulia_scraper/spiders/trulia_sold.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
+import os
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 import trulia_scraper.parsing as parsing
 from trulia_scraper.items import TruliaItem, TruliaItemLoader
 import trulia_scraper.spiders.trulia as trulia
+from scrapy.utils.conf import closest_scrapy_cfg
+
 
 class TruliaSpider(scrapy.Spider):
     name = 'trulia_sold'
     allowed_domains = ['trulia.com']
-    custom_settings = {'FEED_URI': 'data/data_sold_%(state)s_%(city)s_%(time)s.jl', 'FEED_FORMAT': 'jsonlines'}
+    custom_settings = {'FEED_URI': os.path.join(os.path.dirname(closest_scrapy_cfg()), 'data/data_sold_%(state)s_%(city)s_%(time)s.jl'), 
+                       'FEED_FORMAT': 'jsonlines'}
 
     def __init__(self, state='CA', city='San_Francisco', *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,8 +22,8 @@ class TruliaSpider(scrapy.Spider):
         self.le = LinkExtractor(allow=r'^https://www.trulia.com/homes/.+/sold/')
 
     def parse(self, response):
-        pagination = response.css('.paginationContainer').xpath('.//*/text()[contains(., "Results")]').extract_first()
-        N = parsing.get_number_of_pages_to_scrape(pagination)
+        N = trulia.TruliaSpider.get_number_of_pages_to_scrape(response)
+        self.logger.info("Determined that property pages are contained on {N} different index pages, each containing at most 30 properties. Proceeding to scrape each index page...")
         for url in [response.urljoin("{n}_p/".format(n=n)) for n in range(1, N+1)]:
             yield scrapy.Request(url=url, callback=self.parse_index_page)
 
